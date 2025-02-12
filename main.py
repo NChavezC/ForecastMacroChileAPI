8d824b843fe9b35412513fbcfec645f0f65e1eb6
NChavezC <gchavez@fen.uchile.cl>
2025-02-12 19:03:16 -0300

    render.com cors

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Data(BaseModel):
 
 app = FastAPI(debug=True)
 
-origins = ["http://localhost:5173"]
+origins = ["http://localhost:5173", "https://forecastmacrochileapi.onrender.com/docs"]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
